f01b5c95ef97aaf77d321760661143b7bc5753e9
lulungsatrioprayuda <alungsatrio12@gmail.com>
2020-11-06 21:58:57 +0700

    update user

--- resources/views/pages/admin/user/edit.blade.php
+++ resources/views/pages/admin/user/edit.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.admin')
 
 @section('title')
-    Category    
+    User    
 @endsection
 
 @section('content')
 <div class="section-content section-dashboard-home" data-aos="fade-up">
           <div class="container-fluid">
             <div class="dashboard-heading">
-              <h2 class="dashboard-title">Category</h2>
+              <h2 class="dashboard-title">User</h2>
               <p class="dashboard-subtitle">
-                Edit Category
+                Edit User
               </p>
             </div>
             <div class="dashboard-content">
@@ -27,22 +27,39 @@
                     @endif
                     <div class="card">
                         <div class="card-body">
-                        <form action="{{route('category.update', $item->id)}}" method="POST" enctype="multipart/form-data">
+                        <form action="{{route('user.update', $item->id)}}" method="POST" enctype="multipart/form-data">
                             @method('PUT')
                             @csrf
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="">Nama Kategori</label>
-                                    <input type="text" name="name" class="form-control" value="{{$item->name}}" required>
+                                        <label for="">Nama User</label>
+                                        <input type="text" name="name" class="form-control" required value="{{$item->name}}">
                                     </div>
                                 </div>
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="">Photo</label>
-                                        <input type="file" name="photo" class="form-control">
+                                        <label for="">Email User</label>
+                                        <input type="email" name="email" class="form-control" required value="{{$item->email}}">
                                     </div>
                                 </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="">Password User</label>
+                                        <input type="password" name="password" class="form-control">
+                                        <small>Kosongkan jika tidak ingin mengganti password</small>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="">Role</label>
+                                        <select name="roles" class="form-control">
+                                        <option value="{{$item->roles}}" selected>Tidak ganti</option>
+                                            <option value="ADMIN">Admin</option>
+                                            <option value="USER">User</option>
+                                        </select>
+                                    </div>
+                                </div>  
                             </div>
                             <div class="row">
                                 <div class="col text-right">

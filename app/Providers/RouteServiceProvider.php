f6aba4f447c4658a0f27637a3ffb0878d44cda54
lulungsatrioprayuda <alungsatrio12@gmail.com>
2021-01-22 18:45:06 +0700

    update  navbar and login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    // public const gunanya untuk default routes seperti di codeigniter 
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -17,13 +17,57 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">Rewards</a>
                     </li>
+                    @guest
                     <li class="nav-item">
-                        <a class="nav-link" href="/register.html">Sign Up</a>
+                    <a class="nav-link" href="{{route('register')}}">Sign Up</a>
                     </li>
                     <li class="nav-item">
-                        <a class="btn btn-success nav-link px-4 text-white" href="/login.html">Sign In</a>
+                        <a class="btn btn-success nav-link px-4 text-white" href="{{route('login')}}">Sign In</a>
                     </li>
+                    @endguest
                 </ul>
+
+            @auth
+            <!-- Desktop Menu -->
+            <ul class="navbar-nav d-none d-lg-flex">
+                <li class="nav-item dropdown">
+                <a href="#" class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown" >
+                    <img src="/images/icon-user.png" alt="" class="rounded-circle mr-2 profile-picture" />
+                    Hi, {{Auth::user()->name}}
+                </a>
+                <div class="dropdown-menu">
+                <a  class="nav-link" class="dropdown-item">Dashboard</a>
+                    <a href="{{route('dashboard-settings-account')}}" class="dropdown-item" >Settings</a>
+                    <div class="dropdown-divider"></div>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item">Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                </div>
+                </li>
+                <li class="nav-item">
+                <a href="#" class="nav-link d-inline-block mt-2">
+                    <img src="/images/icon-cart-empty.svg" alt="" />
+                </a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav d-block d-lg-none">
+                <li class="nav-item">
+                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">
+                    Hi, {{ Auth::user()->name }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                </li>
+                <li class="nav-item">
+                <a href="#" class="nav-link d-inline-block">
+                    Cart
+                </a>
+                </li>
+            </ul>
+                @endauth
             </div>
         </div>
     </nav>
